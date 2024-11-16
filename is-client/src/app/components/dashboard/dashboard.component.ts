da0a38ee06dcbf81623ce626e87925c022c5e18b
Steve Curtis <steve.curtis@truthmd.com>
2024-11-16 18:10:52 -0500

    dashboard changes

--- is-client/src/app/components/dashboard/dashboard.component.ts
+++ is-client/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { Study } from "../../model/study";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns: string[] = ['name', 'method', 'description'];
+  // displayedColumns: string[] = ['name', 'description'];
+  dataSource: MatTableDataSource<Study> = new MatTableDataSource<Study>();
+
+  private studies: Study[];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.getStudies();
+    this.resetDatasource();
+  }
+
+  private resetDatasource() {
+    this.dataSource.data = this.studies;
+    this.dataSource.sort = this.sort;
+  }
+
+  private getStudies() {
+    this.studies = [
+      {
+        "name": "name",
+        "method": "method",
+        "description": "description"
+      }
+    ]
+  }
 
 }
---
+++ is-client/src/app/model/study.ts
@@ -0,0 +1,5 @@
+export class Study {
+  name: string;
+  method: string;
+  description: string;
+}
