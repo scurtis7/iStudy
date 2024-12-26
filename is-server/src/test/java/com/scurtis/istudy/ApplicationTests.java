7f2a0f897a4199a45b7fb167fad2ded2766d2ade
Steve Curtis <steve.curtis@truthmd.com>
2024-12-26 07:46:00 -0500

    update jave version and dependencies

--- is-server/src/test/java/com/scurtis/istudy/ApplicationTests.java
+++ is-server/src/test/java/com/scurtis/istudy/ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.scurtis.istudy;
 
+import com.scurtis.istudy.service.StudyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
 class ApplicationTests {
+
+    @Autowired
+    private StudyService studyService;
 
     @Test
     void contextLoads() {
+        assertNotNull(studyService);
     }
 
 }
